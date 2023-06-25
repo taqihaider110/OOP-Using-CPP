6f7be27d161c201583df90f3a68def236dc067be
Taqi haider <taqihaider591@gmail.com>
2023-06-25 21:06:12 +0500

    Q3

---
+++ LAB ASSIGNMENT/Lab9/Q3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+class Vehicle{
+    int CarId;
+    string brand,model;
+    public:
+    int carid;
+    string b,m;
+    Vehicle():CarId(carid),brand(b),model(m){}
+    virtual void Is_Available()=0;
+    virtual void rent()=0;
+};
+class 
+int main(){
+    
+    return 0;
+}

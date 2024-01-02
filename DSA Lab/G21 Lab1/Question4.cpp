3a2770b5ad864cd5414835a6b0e30b19ebcb1a64
taqihaider591 <taqihaider591@gmail.com>
2024-01-02 15:48:16 +0500

    push

--- DSA Lab/G21 Lab1/Question4.cpp
+++ DSA Lab/G21 Lab1/Question4.cpp
@@ -1,95 +1,95 @@
 #include<iostream>
 using namespace std;
 class matrix{
-int **p;
-int r;
-int c;
-int *rowmajor;
-int *multiply1D;
-public:
-
-matrix(int row, int col);
-void disp2D(){
-        cout<<"2D matrix element\t";
-        for(int i=0;i<r;i++){
-                for(int j=0;j<c;j++){
-                        cout<<p[i][j]<<" ";
-                }
-                cout<<endl;
-        }
-}
-void dispRowMajor(){
-        cout<<"row major order matrix"<<endl;
-        for(int i=0;i<r*c;i++){
-                cout<<rowmajor[i]<<" ";
-        }
-        cout<<endl;
-}
-void Multiply_rowMajor(matrix & x){
-        if(c!=x.r){
-                cout<<"multiplication not possible";
-                return;
-        }
-        delete multiply1D;
-        multiply1D= new int[r*x.c];
-        for(int i=0;i<r;i++){
-                for(int j=0;j<x.c;j++){
-                        multiply1D[i*x.c+j]=0;
-                        for(int k=0;k<c;k++){
-                                multiply1D[i*x.c+j]+=rowmajor[i*c+k]*x.rowmajor[k*x.c+j];
-                        }
-                }
-        }
-}
-void rowMajor_2D(){
-        cout<<"result of 2D in row major"<<endl;
-        int C = sizeof(multiply1D)/2;
-        for(int i=0;i<r;i++){
-        	for(int j=0;j<C;j++){
-            cout<<multiply1D[i*C+j]<<" ";
-			}
-			cout<<endl;
-        }
-}
-~matrix(){
-        for(int i=0;i<r;i++){
-                delete[] p[i];
-        }
-        delete[] p;
-        delete[] rowmajor;
-        delete[] multiply1D;
-}
+	int **p;
+	int r;
+	int c;
+	int *rowmajor;
+	int *multiply1D;
+	public:
+		
+		matrix(int row, int col);
+		void disp2D(){
+		 cout<<"2D matrix element\t";
+		 for(int i=0;i<r;i++){
+		  for(int j=0;j<c;j++){
+		   cout<<p[i][j]<<" ";
+		  }
+ 		  cout<<endl;
+ 		 }
+		}
+		void dispRowMajor(){
+		 cout<<"row major order matrix"<<endl;
+		 for(int i=0;i<r*c;i++){
+		  cout<<rowmajor[i]<<" ";
+		 }
+		 cout<<endl;
+		}
+		void Multiply_rowMajor(matrix & x){
+		 if(c!=x.r){
+		  cout<<"multiplication not possible";
+		  return;
+		 }
+		 for(int i=0;i<r;i++){
+		  for(int j=0;j<c;j++){
+		   multiply1D[i*x.c+j]=0;
+		   for(int k=0;k<c;k++){
+		    multiply1D[i*x.c+j]+=rowmajor[i*c+k]*x.rowmajor[k*x.c+j];
+		   }
+		  }
+		 }
+		}
+		void rowMajor_2D(){
+	     int C = sizeof(multiply1D)/r;
+		 cout<<"result of 2D in row major"<<endl;
+		 for(int i=0;i<r;i++){
+		  for(int j=0;j<C;j++){
+		   cout<<multiply1D[i*C+j]<<" ";
+		  }
+		  cout<<endl;
+		 }
+		}
+		~matrix(){
+		 for(int i=0;i<r;i++){
+		  delete[] p[i];
+		 }
+		 delete[] p;
+		 delete[] rowmajor;
+		 delete[] multiply1D;
+		}
 };
+
 int main()
 {
-matrix a(4,3);
-matrix b(3,4);
-a.disp2D();
-a.dispRowMajor();
-b.disp2D();
-b.dispRowMajor();
-a.Multiply_rowMajor(b);
-a.rowMajor_2D();
+	matrix a(4,3);
+	matrix b(3,4);
+	a.disp2D();
+	a.dispRowMajor();
+	b.disp2D();
+	b.dispRowMajor();
+	a.Multiply_rowMajor(b);
+	a.rowMajor_2D();
 }
 
 matrix::matrix(int row,int col){
-
-        r=row;
-        c=col;
-        p = new int*[r];
-        rowmajor = new int[r * c];
+	
+ r=row;
+ c=col;
+ p = new int*[r];
+  rowmajor = new int[r * c];
     multiply1D = new int[r * c];
 
     for (int i = 0; i < r; i++) {
         p[i] = new int[c];
         for (int j = 0; j < c; j++) {
             p[i][j] = (i + j);
-            rowmajor[i * c + j] = p[i][j];
+            rowmajor[i * c + j] = p[i][j]; 
         }
     }
 }
 
 
+//First different approach
 //#include<iostream>
 //using namespace std;
 //
---
+++ DSA Lab/G21 Lab4/Question5.cpp
@@ -0,0 +1,234 @@
+#include<iostream>
+using namespace std;
+
+class node {
+	public:
+		int data;
+		node* next;
+		node(int value) {
+			data=value;
+			next=NULL;
+		}
+		node(int value,node* nxt) {
+			data=value;
+			next=nxt;
+		}
+};
+
+class CLL{
+	node* head=NULL;
+	int nodecount=0;
+	public:
+	void setlist(node* h,int n){
+		head=h;
+		nodecount=n;
+	}
+	void makehalves(CLL &obj){
+		int middleposition = (int)nodecount/2;     //Amount for 1st half
+		int secondhalf = nodecount-middleposition;  //Amount for 2nd half
+		int count=0;
+		node* temp=head;
+		node* prev=temp;
+		while(temp->next!=head&&count<middleposition) {  // prev is last element of first half, temp is head of second half
+			prev=temp;
+			temp=temp->next;
+			count++;
+		}
+		prev->next=head; //detach prev from remaining half, circular link back to start
+		node* secondhead=temp;
+		while(temp->next!=head){
+			temp=temp->next;
+		}
+		temp->next=secondhead;
+		obj.setlist(secondhead,secondhalf);
+	}
+			
+	void appendNode(int value){ //insert at end of list / tail
+		if(head==NULL) {  //Check if Linked List Empty.
+			node* n=new node(value,n);
+			head=n;
+			head->next=head;
+			nodecount++;  
+			return;
+		}
+		else{
+			node* temp=head;
+			while(temp->next!=head) {
+				temp=temp->next;
+			}
+			node* n=new node(value,head);
+			temp->next=n;
+			nodecount++;
+		}
+	}
+	void prependNode(int value){ //insert at start of list / head
+		if(head==NULL) {  //Check if Linked List Empty.
+			node* n=new node(value,n);
+			head=n;
+			head->next=head;
+			nodecount++;   
+			return;
+		}
+		node* n=new node(value,head);
+		node* temp=head;
+		while(temp->next!=head) {
+			temp=temp->next;
+		}
+		temp->next=n;
+		head=n;
+		nodecount++;
+	}
+	void insertNodeAfter(int posvalue,int value){  // insert at position (i+1)
+		if(head==NULL) {  //Check if Linked List Empty.
+			cout<<"Empty List, adding at Head.\n";
+			node* n=new node(value,n);
+			head=n;
+			nodecount++;   
+			return;
+		}
+		if(posvalue>nodecount-1){
+			cout<<"Position more than nodes in list, Inserting at tail.\n";
+			appendNode(value);
+			return;
+		}
+		int count=0;
+		node* temp=head;
+		while(temp->next!=head&&count<posvalue) {
+			temp=temp->next;
+			count++;
+		}
+		node* n=new node(value,temp->next);
+		temp->next=n;
+	}
+	void deleteathead(){
+		node* temp=head;
+		while(temp->next=head){
+			temp=temp->next;
+		}
+		temp->next=head->next;
+	}
+	void deleteattail(){
+		node* temp=head;
+		while(temp->next->next=head){
+			temp=temp->next;
+		}
+		temp->next=head;
+	}
+	void deleteNodeByKey(int value){  // delete by value
+		node* temp=head;
+		node* prev=temp;
+		while(temp->next!=head&&temp->data!=value) {
+			prev=temp;
+			temp=temp->next;
+		}
+		if(temp->data==value){ 
+			prev->next=temp->next;  //skip temp (i.e delete)
+			delete temp;
+		}
+		if(temp->next==head){
+			cout<<"Value not in Linked List.\n";
+			return;
+		}
+	}
+	void updateNodeByKey(int value){  // update by value
+		node* temp=head;
+		while(temp->next!=head&&temp->data!=value) {
+			node* prev=temp;
+			temp=temp->next;
+		}
+		if(temp->data==value){ 
+			cout<<"Enter new value: ";
+			cin>>temp->data;
+		}
+		if(temp->next==head){
+			cout<<"Value not in Linked List.\n";
+			return;
+		}
+	}
+	void print(){
+		node* temp=head;
+		cout<<"[HEAD] ";
+		if(head!=NULL){
+		cout<<temp->data<<"| "<<temp->next<<" -> ";
+		temp=temp->next;
+		while(temp!=head) {
+			cout<<temp->data<<"| "<<temp->next<<" -> ";
+			temp=temp->next;
+		}
+		cout<<"head [TAIL]"<<endl;
+		}
+		else{
+			cout<<"NULL [TAIL]"<<endl;
+		}
+	}
+};
+
+int main(){
+	CLL list,list2;
+	float input=0;
+	int value;
+	while(input!=0.5) {
+		cout<<"----------------------------------------------------\n";
+		cout<<"CURRENT LINKED LIST:\n";
+		list.print();
+		cout<<"----------------------------------------------------\n";
+		cout<<"What would you like to do with the linked list?\n";
+		cout<<"1. Insert\t2. Delete\t3. Update\t4. Halve the Linked List [Halving will end the program]\nEnter 0.5 to Exit\n[Anything else will default to Delete]\n";
+		cin>>input;
+		if(input==1) {
+			cout<<"Enter Value to insert: ";
+			cin>>value;
+			cout<<"Where to Insert in Linked List?\n";
+			cout<<"1. At head\t2. At tail\t3. At specified Position\n[Any other value will default to Insertion at Head]\n";
+			cin>>input;
+			if(input == 2){
+				list.appendNode(value);
+			}
+			else if(input == 3){
+				int pos;
+				cout<<"Enter the Position to insert After: ";
+				cin>>pos;
+				list.insertNodeAfter(pos,value);
+			}
+			else{
+				list.prependNode(value);
+			}
+			
+		}
+		else if(input==0.5){
+			break;
+		}
+		else if(input==3){
+			cout<<"Enter the Value to Update: ";
+			cin>>value;
+			list.updateNodeByKey(value);
+		}
+		else if(input==4){
+			cout<<"Halving list"<<endl;
+			list.makehalves(list2);
+			cout<<"First Half list---------------------------------"<<endl;
+			list.print();
+			cout<<"Second Half list--------------------------------"<<endl;
+			list2.print();
+			return 0;
+		}
+		else{
+			cout<<"Where to Delete from Linked List?\n";
+			cout<<"1. At head\t2. At tail\t3. Delete a specific Value\n[Any other value will default to Deletion from Head]\n";
+			cin>>input;
+			if(input == 2){
+				list.deleteattail();
+			}
+			else if(input == 3){
+				int pos;
+				cout<<"Enter the Value to Delete: ";
+				cin>>value;
+				list.deleteNodeByKey(value);
+			}
+			else{
+				list.deleteathead();
+			}
+		}
+		
+	}
+}
